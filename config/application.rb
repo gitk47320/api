611f3649ef8959d18902c8736a44ace163c36c6f
gitk47320 <j.kashi.37204.privateuse@gmail.com>
2020-09-02 22:24:20 +0900

    fakerとseedの作成

--- config/application.rb
+++ config/application.rb
@@ -34,13 +34,13 @@ module Myapp
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins "*"
-        resource "*",
-          headers: :any,
-          methods: [:get, :post, :options, :head]
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins "*"
+    #     resource "*",
+    #       headers: :any,
+    #       methods: [:get, :post, :options, :head]
+    #   end
+    # end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-article = Article.create(title: 'hoge', content: 'hogehoge')
+# create articles
+50.times do |i| 
+  Article.create(title: Faker::Lorem.sentence, content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+end
+
+# create users
+User.create(name: 'test', email: 'test@example.com', password: 'password')
